9913c7a4d3f91e31ad01d4e36cbaff14de13657d
Lance French <lance@french.me>
2021-02-05 14:05:41 -0800

    Adds solution for simplify path

--- golang/simplify-path.go
+++ golang/simplify-path.go
@@ -1,3 +1,17 @@
 func simplifyPath(path string) string {
-
+	s := strings.Split(path, "/")
+	var res []string
+	for _, v := range s {
+		if v == "" || v == "." {
+			continue
+		}
+		if v == ".." {
+			if len(res) > 0 {
+				res = res[:len(res)-1]
+			}
+			continue
+		}
+		res = append(res, v)
+	}
+	return "/" + strings.Join(res, "/")
 }
