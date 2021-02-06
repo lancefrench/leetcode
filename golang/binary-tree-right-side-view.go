585643db33a535d5c151cd4d0012fd475fb20409
Lance French <lance@french.me>
2021-02-06 13:29:08 -0800

    Adds solution for binary tree right side view

--- golang/binary-tree-right-side-view.go
+++ golang/binary-tree-right-side-view.go
@@ -7,5 +7,19 @@
  * }
  */
 func rightSideView(root *TreeNode) []int {
+	res := make([]int, 0)
+	rightView(root, &res, 0)
+	return res
+}
 
+func rightView(curr *TreeNode, res *[]int, currDepth int) {
+	if curr == nil {
+		return
+	}
+	if currDepth == len(*res) {
+		*res = append(*res, curr.Val)
+	}
+
+	rightView(curr.Right, res, currDepth+1)
+	rightView(curr.Left, res, currDepth+1)
 }
