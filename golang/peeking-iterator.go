6a3ff4c06ec05b302b7ed70521fbfbfc88d8edb0
Lance French <lfrench@vmware.com>
2021-02-08 20:40:35 -0800

    Adds golang solution for peeking-iterator

--- golang/peeking-iterator.go
+++ golang/peeking-iterator.go
@@ -14,20 +14,28 @@
  */
 
 type PeekingIterator struct {
+	current int
+	data    []int
 }
 
 func Constructor(iter *Iterator) *PeekingIterator {
-
+	var res PeekingIterator
+	for iter.hasNext() {
+		res.data = append(res.data, iter.next())
+	}
+	res.current = 0
+	return &res
 }
 
 func (this *PeekingIterator) hasNext() bool {
-
+	return this.current < len(this.data)
 }
 
 func (this *PeekingIterator) next() int {
-
+	this.current++
+	return this.data[this.current-1]
 }
 
 func (this *PeekingIterator) peek() int {
-
+	return this.data[this.current]
 }
