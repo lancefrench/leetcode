7178fbddcd8a7ec98f1e4b4a733bd0a6b6a948e0
Lance French <lance@french.me>
2021-02-10 03:34:32 +0000

    Adds solution for Convert BST to Greater Tree

--- golang/convert-bst-to-greater-tree.go
+++ golang/convert-bst-to-greater-tree.go
@@ -7,5 +7,17 @@
  * }
  */
 func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+	travel(root, &sum)
+	return root
+}
 
+func travel(root *TreeNode, sum *int) {
+	if root == nil {
+		return
+	}
+	travel(root.Right, sum)
+	*sum += root.Val
+	root.Val = *sum
+	travel(root.Left, sum)
 }
