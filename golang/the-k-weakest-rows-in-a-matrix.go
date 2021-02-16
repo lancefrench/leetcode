cb8cc1f2d571297868cf33b8671e392aadaba3c4
Lance French <lance@french.me>
2021-02-15 18:58:14 -0800

    Adds solution for The K Weakeset Rows in a Matrix

--- golang/the-k-weakest-rows-in-a-matrix.go
+++ golang/the-k-weakest-rows-in-a-matrix.go
@@ -1,3 +1,18 @@
 func kWeakestRows(mat [][]int, k int) []int {
+	res := make([]int, 0)
+	for j := 0; j < len(mat[0]); j++ { //iterate column by column
+		for i := 0; i < len(mat); i++ {
+			if j == 0 {
+				mat[i] = append(mat[i], 0) //append 0 at the end to handle all 1s case
+			}
+			if mat[i][j] == 0 && (j == 0 || mat[i][j-1] != 0) { //find first 0 of the row
+				res = append(res, i) //append into output
+				if len(res) == k {
+					return res
+				}
+			}
+		}
+	}
 
+	return res
 }
