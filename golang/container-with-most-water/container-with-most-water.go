419beb22ee64fcf3c7d802c58d7be9cd8ce46a7f
Lance French <lance@french.me>
2021-02-17 19:33:39 -0800

    Adds solution for 0011 Container With Most Water

--- golang/container-with-most-water/container-with-most-water.go
+++ golang/container-with-most-water/container-with-most-water.go
@@ -1,5 +1,32 @@
 package problem0011
 
 func maxArea(height []int) int {
-	return 0
+	left, right := 0, len(height)-1
+	max := 0
+
+	for left < right {
+		x1, x2 := height[left], height[right]
+		y := min(x1, x2)
+
+		area := y * (right - left)
+		if max < area {
+			max = area
+		}
+
+		if x1 < x2 {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return max
+}
+
+// https://stackoverflow.com/questions/27516387/what-is-the-correct-way-to-find-the-min-between-two-integers-in-go
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
