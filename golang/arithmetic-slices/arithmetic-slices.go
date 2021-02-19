b26cc4462e77d367153b6f76ca0bd3e2f13cb996
Lance French <lance@french.me>
2021-02-18 18:43:43 -0800

    Adds solution for 0413 Arithmetic Slices

--- golang/arithmetic-slices/arithmetic-slices.go
+++ golang/arithmetic-slices/arithmetic-slices.go
@@ -1,5 +1,21 @@
 package problem0413
 
 func numberOfArithmeticSlices(A []int) int {
-	return 0
+	res := 0
+	if len(A) < 3 {
+		return res
+	}
+
+	var i, j = 0, 0
+	for i < len(A) {
+		j = i + 2
+		for j < len(A) && A[j]-A[j-1] == A[j-1]-A[j-2] {
+			j++
+		}
+		j--
+		res += (j - i - 1) * (j - i) / 2
+		i = j
+	}
+
+	return res
 }
