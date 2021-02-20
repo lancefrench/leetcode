33d1c570077abdce3ff5902cd0b0632b5fe9d3eb
Lance French <lance@french.me>
2021-02-19 19:29:07 -0800

    Adds test for 1249 Minimum Remove to Make Valid Parentheses

--- golang/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
+++ golang/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
@@ -1,5 +1,6 @@
 package problem1249
 
 func minRemoveToMakeValid(s string) string {
-
+	res := "test"
+	return res
 }
--- golang/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses_test.go
+++ golang/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses_test.go
@@ -1,1 +1,36 @@
 package problem1249
+
+import "testing"
+
+type tc struct {
+	params
+	output
+}
+
+type params struct {
+	one string
+}
+
+type output struct {
+	one string
+}
+
+func Test_Problem1249(t *testing.T) {
+	tcs := []tc{
+		{
+			params{"lee(t(c)o)de)"},
+			output{"lee(t(c)o)de"},
+		},
+	}
+
+	for _, tc := range tcs {
+		o, p := tc.output, tc.params
+
+		got := minRemoveToMakeValid(p.one)
+		want := o.one
+
+		if got != want {
+			t.Errorf("got minRemoveToMakeValid(\"%v\") = \"%v\" want \"%v\"", tc.params, got, want)
+		}
+	}
+}
