2c4d4f4fad191b93de9ac1ebfcf775f02557bc36
Lance French <lance@french.me>
2021-02-19 21:21:57 -0800

    Solution for 1249 Minimum Remove to Make Valid Parentheses

--- golang/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
+++ golang/minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.go
@@ -1,6 +1,33 @@
 package problem1249
 
+import "strings"
+
 func minRemoveToMakeValid(s string) string {
-	res := "test"
-	return res
+	res := make([]string, len(s))
+
+	stack := []int{}
+	for i, r := range s {
+		// convert our unicode rune back to an ascii character
+		c := string(r)
+		switch c {
+		case "(":
+			stack = append(stack, i)
+			res[i] = c
+		case ")":
+			if len(stack) == 0 {
+				res[i] = ""
+				continue
+			}
+			stack = stack[:len(stack)-1]
+			res[i] = c
+		default:
+			res[i] = c
+		}
+	}
+
+	for _, idx := range stack {
+		res[idx] = ""
+	}
+
+	return strings.Join(res, "")
 }
