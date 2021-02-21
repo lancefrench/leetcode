aca5b574b8b0e1d5f86ae3f30cd559eff3acd2f1
Lance French <lfrench@vmware.com>
2021-02-20 20:55:19 -0800

    Adds solition for 0013 Roman to Integer

--- golang/roman-to-integer/roman-to-integer.go
+++ golang/roman-to-integer/roman-to-integer.go
@@ -1,5 +1,33 @@
 package Problem0013
 
+var conversion = map[rune]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
 func romanToInt(s string) int {
-	return 0
+	res := 0
+
+	for i := range s {
+		s1 := conversion[rune(s[i])]
+
+		if i < len(s)-1 {
+			s2 := conversion[rune(s[i+1])]
+
+			if s1 < s2 {
+				res -= s1
+			} else {
+				res += s1
+			}
+		} else {
+			res += s1
+		}
+	}
+
+	return res
 }
