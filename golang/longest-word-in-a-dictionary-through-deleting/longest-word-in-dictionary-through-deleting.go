8e57dfcdd80f9e6fc36c142530de3d9458432dcf
Lance French <lance@french.me>
2021-02-22 20:27:02 -0800

    Adds solution for 0524

--- golang/longest-word-in-a-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.go
+++ golang/longest-word-in-a-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.go
@@ -1,5 +1,26 @@
 package problem0524
 
+import "strings"
+
 func findLongestWord(s string, d []string) string {
-	return "hello"
+	res := ""
+	for _, str := range d {
+		if len(str) > len(res) || len(str) == len(res) && str < res {
+			if isSubstr(s, str) {
+				res = str
+			}
+		}
+	}
+	return res
+}
+
+func isSubstr(s, str string) bool {
+	for _, c := range str {
+		p := strings.Index(s, string(c))
+		if p == -1 {
+			return false
+		}
+		s = s[p+1:]
+	}
+	return true
 }
