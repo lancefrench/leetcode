3d11e6e3be06075ade9a82ae3bc436413300f5af
Lance French <lance@french.me>
2021-02-23 19:37:28 -0800

    Adds solution for Problem 0240 Search a 2d Matrix II

--- golang/search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
+++ golang/search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -1,5 +1,19 @@
 package problem0240
 
 func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	m, n := len(matrix)-1, len(matrix[0])-1
+	i, j := m, 0
+	for i >= 0 && j <= n {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] > target {
+			i--
+		} else {
+			j++
+		}
+	}
 	return false
 }
