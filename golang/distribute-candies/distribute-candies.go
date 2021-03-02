b58600991554c4e9cc662069f09b7ddec87d8470
Lance French <lance@french.me>
2021-03-01 19:51:17 -0800

    Adds solution for problem 0575 Distribute Candies

--- golang/distribute-candies/distribute-candies.go
+++ golang/distribute-candies/distribute-candies.go
@@ -1,5 +1,12 @@
 package problem0575
 
-func distributeCandies(candyType []int) int {
-	return 0
+func distributeCandies(candies []int) int {
+	kinds := make(map[int]int)
+	for _, v := range candies {
+		kinds[v] = kinds[v] + 1
+	}
+	if len(kinds) >= len(candies)/2 {
+		return len(candies) / 2
+	}
+	return len(kinds)
 }
