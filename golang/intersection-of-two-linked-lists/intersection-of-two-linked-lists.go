32b28b86b7ab6c8fae4ad373991d8b7ffdfbbf78
Lance French <lance@french.me>
2021-03-04 22:14:21 -0800

    Adds problem 0160 Intersection of Two Linked Lists

---
+++ golang/intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
@@ -0,0 +1,11 @@
+package problem0160
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	node := new(ListNode)
+	return node
+}
