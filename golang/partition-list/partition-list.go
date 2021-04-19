a26173ec7cee41e1f30b498940a9abff046f1ac8
Lance French <lance@french.me>
2021-04-19 16:48:01 -0700

    Adds solution for Problem0086 Partition List

--- golang/partition-list/partition-list.go
+++ golang/partition-list/partition-list.go
@@ -1,4 +1,4 @@
-package Problem0086
+package problem0086
 
 /**
  * Definition for singly-linked list.
@@ -8,5 +8,25 @@ package Problem0086
  * }
  */
 func partition(head *ListNode, x int) *ListNode {
-	return nil
+	if head == nil || head.Next == nil {
+		return head
+	}
+	lesserHead, greaterHead := &ListNode{}, &ListNode{}
+	lesserCurrent, greaterCurrent := lesserHead, greaterHead
+
+	for head != nil {
+		if head.Val < x {
+			lesserCurrent.Next = head
+			lesserCurrent = lesserCurrent.Next
+		} else {
+			greaterCurrent.Next = head
+			greaterCurrent = greaterCurrent.Next
+		}
+		head = head.Next
+	}
+
+	greaterCurrent.Next = nil
+	lesserCurrent.Next = greaterHead.Next
+
+	return lesserHead.Next
 }
