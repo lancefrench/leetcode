472ee0eb1f9f776ecf15856d971c5734f7f240cf
Lance French <lance@french.me>
2021-04-21 20:46:43 -0700

    Adds untested solution for problem 0377 Combination Sum IV

--- golang/combination-sum-iv/combination-sum-iv.go
+++ golang/combination-sum-iv/combination-sum-iv.go
@@ -1,3 +1,20 @@
-func combinationSum4(nums []int, target int) int {
+package problem0377
 
+func combinationSum4(nums []int, target int) int {
+	solutions := make([]int, target+1)
+
+	solutions[0] = 1
+
+	for i := 1; i <= target; i++ {
+		for _, num := range nums {
+			// if
+			if i-num < 0 {
+				continue
+			}
+
+			solutions[i] += solutions[i-num]
+		}
+	}
+
+	return solutions[target]
 }
