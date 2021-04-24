9f215320e6d8b52d1368f06deb87654486b3821f
Lance French <lance@french.me>
2021-04-24 16:36:29 -0700

    Adds solution for problem 0008 String to Integer (atoi)

--- golang/string-to-integer-atoi/string-to-integer-atoi.go
+++ golang/string-to-integer-atoi/string-to-integer-atoi.go
@@ -1,5 +1,44 @@
 package problem0008
 
+import "math"
+
 func myAtoi(s string) int {
-	return 0
+	sign := 1
+	res := 0
+	var i int
+
+	if len(s) == 0 {
+		return res
+	}
+
+	for i = 0; i < len(s); i++ {
+		switch {
+		case s[i] == ' ':
+			continue
+		case s[i] == '+':
+			i++
+			break
+		case s[i] == '-':
+			sign = -1
+			i++
+			break
+		case s[i] < '0' || s[i] > '9':
+			return 0
+		}
+		break
+	}
+
+	for ; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			break
+		}
+		res = res*10 + int(s[i]-'0')
+		if res > math.MaxInt32 {
+			if sign == 1 {
+				return math.MaxInt32
+			}
+			return -math.MaxInt32 - 1
+		}
+	}
+	return sign * res
 }
