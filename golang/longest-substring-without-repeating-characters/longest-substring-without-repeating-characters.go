450434a5e7d5727c6e96d58db78b244506586102
Lance French <lance@french.me>
2021-04-24 13:01:19 -0700

    Adds comments for 0003

--- golang/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ golang/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -4,14 +4,20 @@ func lengthOfLongestSubstring(s string) int {
 	asciiSet := [128]int{}
 	max := 0
 	for left, right := 0, 0; right < len(s); right++ {
+		// if the last occurance of the character is greater than our left
+		// window index, move the window forward so that we can never contain
+		// a duplicate character
 		if asciiSet[s[right]] > left {
 			left = asciiSet[s[right]]
 		}
 
+		// Update max length if needed
 		if right-left+1 > max {
 			max = right - left + 1
 		}
 
+		// Set the value tracking our right index to one beyond
+		// the last occurance of this character
 		asciiSet[s[right]] = right + 1
 	}
 	return max
