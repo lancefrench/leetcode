f01031c52fe3b03ae71c4087e5f0285e7f55367f
Lance French <lance@french.me>
2021-04-23 19:23:58 -0700

    Adds solution for problem 0003 Longest Substring Without Repeating Characters

--- golang/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ golang/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,5 +1,18 @@
 package problem0003
 
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	asciiSet := [128]int{}
+	max := 0
+	for left, right := 0, 0; right < len(s); right++ {
+		if asciiSet[s[right]] > left {
+			left = asciiSet[s[right]]
+		}
+
+		if right-left+1 > max {
+			max = right - left + 1
+		}
+
+		asciiSet[s[right]] = right + 1
+	}
+	return max
 }
