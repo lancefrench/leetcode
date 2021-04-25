0103969df421c67dd057249de80ef05b51aad016
Lance French <lance@french.me>
2021-04-25 16:42:04 -0700

    First pass at a solution reusing my solution for 2sum.  Not accepted due to sorting.

--- golang/3sum/3sum.go
+++ golang/3sum/3sum.go
@@ -1,5 +1,35 @@
 package problem0015
 
-func threeSum(nums []int) [][]int {
+import (
+	"sort"
+)
 
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := make([][]int, 0)
+
+	for i := range nums {
+		if nums[i] > 0 {
+			break
+		}
+		if i == 0 || nums[i-1] != nums[i] {
+			twoSum(nums, i, &res)
+		}
+	}
+	return res
+}
+
+func twoSum(nums []int, i int, res *[][]int) {
+	seen := make(map[int]bool)
+	j := i + 1
+	for ; j < len(nums); j++ {
+		complement := -nums[i] - nums[j]
+		if seen[complement] {
+			*res = append(*res, []int{nums[i], nums[j], complement})
+			for j+1 < len(nums) && nums[j] == nums[j+1] {
+				j += 1
+			}
+		}
+		seen[nums[j]] = true
+	}
 }
