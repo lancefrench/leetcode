3ea53234b9eab7821439e9bdcf0a7ddfd4fddb9c
Lance French <lance@french.me>
2021-04-30 21:29:57 -0700

    Adds solution for problem0016 3Sum Closest

--- golang/3sum-closest/3sum-closest.go
+++ golang/3sum-closest/3sum-closest.go
@@ -1,5 +1,37 @@
 package problem0016
 
+import (
+	"math"
+	"sort"
+)
+
 func threeSumClosest(nums []int, target int) int {
-	return 0
+	diff := math.MaxInt32
+	sort.Ints(nums)
+
+	for i, _ := range nums {
+		low, high := i+1, len(nums)-1
+		for low < high {
+			sum := nums[i] + nums[low] + nums[high]
+			if abs(target-sum) < abs(diff) {
+				diff = target - sum
+			}
+			if sum < target {
+				low += 1
+			} else {
+				high -= 1
+			}
+			if diff == 0 {
+				break
+			}
+		}
+	}
+	return target - diff
+}
+
+func abs(x int) int {
+	if x < 0 {
+		x = -x
+	}
+	return x
 }
