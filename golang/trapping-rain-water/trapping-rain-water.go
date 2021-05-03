311410258460ad52d9dc81fa170afd7a59a429a8
Lance French <lance@french.me>
2021-05-03 11:28:31 -0700

    Adds solution for problem0042 Trapping Rain Water

--- golang/trapping-rain-water/trapping-rain-water.go
+++ golang/trapping-rain-water/trapping-rain-water.go
@@ -1,5 +1,53 @@
 package problem0042
 
+type stack []int
+
+func (s *stack) Push(x int) {
+	*s = append(*s, x)
+}
+
+func (s *stack) Pop() (int, bool) {
+	l := len(*s)
+	if l == 0 {
+		return 0, false
+	}
+
+	i := l - 1
+	x := (*s)[i]
+	*s = (*s)[:i]
+
+	return x, true
+}
+
+func (s *stack) Peek() int {
+	if len(*s) == 0 {
+		return 0
+	}
+	return (*s)[len(*s)-1]
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
 func trap(height []int) int {
-	return 0
+	var stack stack
+	var res int
+
+	for i := 0; i < len(height); i++ {
+		for len(stack) != 0 && height[i] > height[stack.Peek()] {
+			top, _ := stack.Pop()
+			if len(stack) == 0 {
+				break
+			}
+			distance := i - stack.Peek() - 1
+			boundedHeight := min(height[i], height[stack.Peek()]) - height[top]
+			res += distance * boundedHeight
+		}
+		stack.Push(i)
+	}
+	return res
 }
