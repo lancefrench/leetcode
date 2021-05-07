94f5b22f0950237ec6bee5a5a7f8c8067f4f3038
Lance French <lance@french.me>
2021-05-07 15:39:05 -0700

    Adds solution for problem0045 Jump Game II

--- golang/jump-game-ii/jump-game-ii.go
+++ golang/jump-game-ii/jump-game-ii.go
@@ -1,5 +1,20 @@
 package problem0045
 
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
 func jump(nums []int) int {
-	return 0
+	jumps, currentJumpEnd, farthest := 0, 0, 0
+	for i := 0; i < len(nums)-1; i++ {
+		farthest = Max(farthest, i+nums[i])
+		if i == currentJumpEnd {
+			jumps++
+			currentJumpEnd = farthest
+		}
+	}
+	return jumps
 }
