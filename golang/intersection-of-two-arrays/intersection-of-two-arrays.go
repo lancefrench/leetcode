277a0beaf3d4a2b99f983047ee52f3fbaaf4df7f
Lance French <lance@french.me>
2021-05-09 19:00:42 -0700

    Adds initial tests for problem 0349 Intersection of Two Arrays

--- golang/intersection-of-two-arrays/intersection-of-two-arrays.go
+++ golang/intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -1,3 +1,5 @@
-func intersection(nums1 []int, nums2 []int) []int {
+package problem0349
 
+func intersection(nums1 []int, nums2 []int) []int {
+	return nums1
 }
---
+++ golang/intersection-of-two-arrays/intersection-of-two-arrays_test.go
@@ -0,0 +1,52 @@
+package problem0349
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type tc struct {
+	params
+	output
+}
+
+type params struct {
+	nums1 []int
+	nums2 []int
+}
+
+type output struct {
+	ans []int
+}
+
+func TestProblem0349(t *testing.T) {
+	tcs := []tc{
+		{
+			params{
+				[]int{1, 2, 2, 1},
+				[]int{2, 2},
+			},
+			output{
+				[]int{2},
+			},
+		},
+		{
+			params{
+				[]int{4, 9, 5},
+				[]int{9, 4, 9, 8, 4},
+			},
+			output{
+				[]int{4, 9},
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		p, o := tc.params, tc.output
+
+		expected := o.ans
+		actual := intersection(p.nums1, p.nums2)
+		assert.Equal(t, expected, actual)
+	}
+}
