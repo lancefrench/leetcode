dd3c04274340bd639cc90710037522a8ee0dbb5a
Lance French <lance@french.me>
2021-05-27 13:08:43 -0700

    Adds solution for problem0053 Maximum Subarray

--- golang/maximum-subarray/maximum-subarray.go
+++ golang/maximum-subarray/maximum-subarray.go
@@ -1,5 +1,21 @@
 package problem0053
 
+import (
+	"math"
+)
+
 func maxSubArray(nums []int) int {
-	return 0
+	max_so_far, max_ending_here := (-math.MaxInt32 - 1), 0
+
+	for i := 0; i < len(nums); i++ {
+		max_ending_here = max_ending_here + nums[i]
+		if max_so_far < max_ending_here {
+			max_so_far = max_ending_here
+		}
+
+		if max_ending_here < 0 {
+			max_ending_here = 0
+		}
+	}
+	return max_so_far
 }
