5596498f4db9d651f741275e23f3dd38582e0d32
Lance French <lfrench@vmware.com>
2021-06-02 12:37:33 -0700

    Adds solution for problem 0695 Max Area Of Island

--- golang/max-area-of-island/max-area-of-island.go
+++ golang/max-area-of-island/max-area-of-island.go
@@ -1,5 +1,29 @@
 package problem0695
 
+// A Depth-First Search (Iterative) approach
+// TODO avoid searching squares we've already counted
+
 func maxAreaOfIsland(grid [][]int) int {
-	return 0
+	var res int
+	for row := range grid {
+		for col := range grid[row] {
+			area := traverseIsland(grid, row, col)
+			if res < area {
+				res = area
+			}
+		}
+	}
+	return res
+}
+
+func traverseIsland(grid [][]int, row, col int) int {
+	if row < 0 || row >= len(grid) || col < 0 || col >= len(grid[row]) || grid[row][col] == 0 {
+		return 0
+	}
+	grid[row][col] = 0
+	res := traverseIsland(grid, row+1, col)
+	res += traverseIsland(grid, row-1, col)
+	res += traverseIsland(grid, row, col+1)
+	res += traverseIsland(grid, row, col-1)
+	return res + 1
 }
