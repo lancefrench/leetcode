b6ec9a1ca937679450ed8250cf2d0305652c5cad
Lance French <lfrench@vmware.com>
2023-10-13 13:32:57 +1300

    Adds problem 0124 Binary Tree Maximum Path Sum

---
+++ golang/binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
@@ -0,0 +1,13 @@
+package Problem0124
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxPathSum(root *TreeNode) int {
+
+}
